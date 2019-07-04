f7a342f1278c39d27890b0a34b7cd2679079eb42
TeslaFeo <murat.kataki@gmail.com>
2019-07-04 13:19:07 +0300

    error_reporting

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if ( DEBUG_MODE ) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 } else {
-    error_reporting(-1);
+    error_reporting(0);
     ini_set('display_errors', 0);
 }
 
